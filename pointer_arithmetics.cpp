e85ba68d8134f173d738a0a83e7e10e8e4ffda7f
lagcom <laggingcomputer2000@gmail.com>
2025-02-17 19:48:06 -0500

    Make it better able to deal with different variable sizes

--- pointer_arithmetics.cpp
+++ pointer_arithmetics.cpp
@@ -3,12 +3,12 @@
 #include<stdlib.h>
 
 int main(int argc, char*argv[]){
-    char hexFormat[][9]={"%02x ","%04x ","%08x ","%016lx ","%016llx "};
+    char hexFormat[][9]={"%02x ","%04x ","%08x ","%016lx ","%016llx ","%016llx"};
     char decimalFormat[][9]={"%d ","%d ","%d ","%ld ","%lld "};
-    char (*format)[9];
+    char (*format)[9]=hexFormat;
     char str[100000]="This is a c-string of length 31";
-    int intArray[100000]={1,2,3,4,5,6,7,8};
-    double doubleArray[100000]={1.1,2.2,3.3,4.4};
+    int intArray[100000]={1,2,3,4,5,6,7,8}, intArraySize = 8;
+    double doubleArray[100000]={1.1,2.2,3.3,4.4}, doubleArraySize = 4;
     int length = strlen(str)+1;
     void *start = str;
     switch(argc){
@@ -24,12 +24,53 @@ int main(int argc, char*argv[]){
 
         default:
             if(*argv[1]=='d') format = decimalFormat;
-            else format = hexFormat;
+            else {
+                char number[3]="2";
+
+                strcpy(format[0],"%0");
+                sprintf(number,"%lu",sizeof(char)*2);
+                strcat(format[0],number);
+                strcat(format[0],"x ");
+                
+                strcpy(format[1],"%0");
+                sprintf(number,"%lu",sizeof(short)*2);
+                strcat(format[1],number);
+                strcat(format[1],"x ");
+                
+                strcpy(format[2],"%0");
+                sprintf(number,"%lu",sizeof(int)*2);
+                strcat(format[2],number);
+                strcat(format[2],"x ");
+                
+                strcpy(format[3],"%0");
+                sprintf(number,"%lu",sizeof(long)*2);
+                strcat(format[3],number);
+                strcat(format[3],"lx ");
+                
+                strcpy(format[4],"%0");
+                sprintf(number,"%lu",sizeof(long long)*2);
+                strcat(format[4],number);
+                strcat(format[4],"llx ");
+
+                strcpy(format[5],"%0");
+                sprintf(number,"%lu",sizeof(long long)*2);
+                strcat(format[5],number);
+                strcat(format[5],"llx");
+            }
 
             if(argc == 2) break;
 
-            if(*argv[2]=='i') start=(char*)intArray;
-            else if (*argv[2]=='f') start=(char*)doubleArray;
+            if(*argv[2]=='i') {
+                start=(char*)intArray;
+                length=intArraySize*sizeof(int);
+            }
+            else if (*argv[2]=='f') {
+                start=(char*)doubleArray;
+                length=doubleArraySize*sizeof(double);
+            }
+            else{
+                length*=sizeof(char);
+            }
             
             if(argc == 3) break;
 
@@ -38,14 +79,14 @@ int main(int argc, char*argv[]){
                 for(i=0 ; i+3<argc ; i++){
                     intArray[i] = atoi(argv[i+3]);
                 }
-                length = (argc-3)*4;
+                length = (argc-3)*sizeof(int);
             }
             else if(*argv[2]=='f'){
                 int i;
                 for(i=0 ; i+3<argc ; i++){
                     doubleArray[i] = atof(argv[i+3]);
                 }
-                length = (argc-3)*8;
+                length = (argc-3)*sizeof(double);
             }
             else{
                 int i;
@@ -57,6 +98,7 @@ int main(int argc, char*argv[]){
                     strcat(str,argv[i]);
                 }
                 length = strlen(str);
+                length *= sizeof(char);
             }
     }
 
@@ -64,9 +106,9 @@ int main(int argc, char*argv[]){
     void pointer to store a generic address without a data type
     this can be compared with other pointer types without casting
     */
-    const void*end = start+length;
+    const void*end = (void*)((unsigned long)start+length);
 
-    printf("printing as a string\n%s\n",start);
+    printf("printing as a string\n%s\n",(char*)start);
 
     #if (defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199409L)) || defined(__cplusplus)
     
@@ -114,13 +156,10 @@ int main(int argc, char*argv[]){
     __int128_t * _128p;
     printf("printing as a 128-bit integer array, size %lu\n",sizeof(__int128_t));
     for(_128p=(__int128_t*)start;_128p<end;_128p++){
-        if(sizeof(long long)<<1 == sizeof(__int128_t))
-            printf("%016llx%016llx ",
-                *(((long long*)_128p)+1),
-                *((long long*)_128p)
-                );
-        else
-            printf("%016llx ",*_128p);
+        int i=(sizeof(__int128_t)/sizeof(long long));
+        while(i--)
+            printf(format[5],*(((long long*)_128p)+i));
+        printf(" ");
     }
     printf("\n");
     #endif
